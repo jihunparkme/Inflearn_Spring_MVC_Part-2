c7b2181068bea20938b90d7511324fccfebee088
jihun.park <jihunpark.tech@gmail.com>
2021-11-11 22:43:10 +0900

    [검증1 - Validation] 오류 코드와 메시지 처리4

---
+++ validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,30 @@
+package hello.itemservice.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MessageCodesResolverTest {
+
+    // MessageCodesResolver : 인터페이스, DefaultMessageCodesResolver : 기본 구현체
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
